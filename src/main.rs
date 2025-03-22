e5983214a1331f1ed3e6910d8f6c01507c7dd3ef
Juani <juanischuhmann@gmail.com>
2025-03-22 18:11:53 -0300

    ultra ugly wip suggestions

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,47 @@ fn main() {
         .text()
         .expect("failed to get text");
     let doc_body = scraper::Html::parse_document(&resp);
-    let definitions = scraper::Selector::parse("div.def.ddef_d.db").expect("failed to create selector");
+    let definitions =
+        scraper::Selector::parse("div.def.ddef_d.db").expect("failed to create selector");
     let list_of_definitions = doc_body
         .select(&definitions)
         .map(|d| d.text().collect())
         .collect::<Vec<String>>();
     if list_of_definitions.is_empty() {
         println!("Not found");
+        // https://dictionary.cambridge.org/spellcheck/english/?q=helo
+        let suggestions =
+            scraper::Selector::parse("li.lbt.lp-5.lpl-20 > a").expect("failed to create selector");
+        let list_of_suggestions = doc_body
+            .select(&suggestions)
+            .map(|d| d.inner_html())
+            .collect::<Vec<String>>();
+            println!("Perhaps you wanted to say:");
+            for (i, suggestion) in list_of_suggestions.iter().enumerate() {
+                println!("  {}: {}", i + 1, suggestion);
+            } 
         return;
     }
     println!("Definitions found for \"{}\":", args[1..].join(" "));
     for (i, definition) in list_of_definitions.iter().enumerate() {
-        println!("  {}: {}", i+1, &definition[0..definition.len() - 2]);
+        println!("  {}: {}", i + 1, &definition[0..definition.len() - 2]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, io::Read};
+
+    #[test]
+    fn it_works() {
+        let mut file = File::open("./src/test.html").expect("failed to open file");
+        let mut data = String::with_capacity(10000);
+        file.read_to_string(&mut data).expect("failed to read to string");
+        let suggestions = scraper::Selector::parse("li.lbt.lp-5.lpl-20 > a").expect("failed to create selector");
+        let doc = scraper::Html::parse_document(&data);
+        let res = doc.select(&suggestions)
+        .map(|d| d.inner_html())
+        .collect::<Vec<String>>();
+        assert_eq!(res.get(0).map(|f| f.clone()), Some("hello".to_string()));
     }
 }
