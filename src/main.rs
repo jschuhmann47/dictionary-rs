d477686377b538c201d2cfc652693c2ec4978a67
Juani <juanischuhmann@gmail.com>
2025-03-29 13:23:41 -0300

    rename fns

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() {
         "https://dictionary.cambridge.org/dictionary/english/{}",
         query
     );
-    let list_of_definitions = query_url_with_selector(&url, DEFINITIONS_SELECTOR);
+    let list_of_definitions = get_selectors_from_url(&url, DEFINITIONS_SELECTOR);
 
     if list_of_definitions.is_empty() {
         println!("Not found");
@@ -24,7 +24,7 @@ fn main() {
             "https://dictionary.cambridge.org/spellcheck/english/?q={}",
             query
         );
-        let list_of_suggestions = query_url_with_selector(&url, SUGGESTIONS_SELECTOR);
+        let list_of_suggestions = get_selectors_from_url(&url, SUGGESTIONS_SELECTOR);
         println!("Perhaps you wanted to say:");
         for (i, suggestion) in list_of_suggestions.iter().enumerate() {
             println!("  {}: {}", i + 1, suggestion);
@@ -33,11 +33,12 @@ fn main() {
     }
     println!("Definitions found for \"{}\":", args[1..].join(" "));
     for (i, definition) in list_of_definitions.iter().enumerate() {
+        // the len - 2 is to truncate the ending that comes like "foo: " to just "foo"
         println!("  {}: {}", i + 1, &definition[0..definition.len() - 2]);
     }
 }
 
-fn get_from_url_as_html(url: &str) -> Html {
+fn get_url_as_html(url: &str) -> Html {
     let res = reqwest::blocking::get(url)
         .expect("failed to get url")
         .text()
@@ -45,14 +46,14 @@ fn get_from_url_as_html(url: &str) -> Html {
     scraper::Html::parse_document(&res)
 }
 
-fn get_from_selector(body: &Html, selector: Selector) -> Vec<String> {
+fn get_selectors(body: &Html, selector: Selector) -> Vec<String> {
     body.select(&selector)
         .map(|d| d.text().collect())
         .collect::<Vec<String>>()
 }
 
-fn query_url_with_selector(url: &str, selector: &str) -> Vec<String> {
-    let doc_body = get_from_url_as_html(&url);
+fn get_selectors_from_url(url: &str, selector: &str) -> Vec<String> {
+    let doc_body = get_url_as_html(&url);
     let suggestions = scraper::Selector::parse(selector).expect("failed to create selector");
-    get_from_selector(&doc_body, suggestions)
+    get_selectors(&doc_body, suggestions)
 }
